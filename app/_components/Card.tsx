56c6a7e4a3f587c6dad3c8cf1120297a9a82fbf3
SouadAlsayed <souadalsayed1804@gmail.com>
2025-12-01 18:49:53 +0200

    feat(ui): enhance Card component with image quality and layout adjustments and  update Shop section title and add 'View All Products' button

--- app/_components/Card.tsx
+++ app/_components/Card.tsx
@@ -11,6 +11,7 @@ function Card({ product }: CardProps) {
 
   return (
     <div className="w-full max-w-[300px] mx-auto bg-white rounded-2xl py-6 px-10 shadow-lg hover:shadow-xl transition-all duration-300 transform hover:-translate-y-2 relative">
+      {/* Favourite */}
       <button className="absolute top-3 right-3 transition group">
         <Heart
           size={20}
@@ -20,16 +21,29 @@ function Card({ product }: CardProps) {
         />
       </button>
 
+      {/* Image */}
       <div className="relative w-full h-70 md:h-90 rounded-xl overflow-hidden mb-4">
-        <Image src={image} alt={name} fill className="object-cover" />
+        <Image
+          src={image}
+          alt={name}
+          fill
+          quality={100}
+          className="object-cover"
+        />
       </div>
 
+      {/* Content */}
       <div className="flex flex-col justify-between items-center text-center gap-2 px-2">
         <h3 className="font-semibold text-lg ">{name}</h3>
-        <p className="text-gray-600 text-sm">{description}</p>
 
-        <p className="font-semibold text-gray-800">120.00 LE</p>
-        <button className="w-full flex items-center justify-center gap-2 bg-button-accent hover:bg-button-accent-hover text-white px-4 py-2 rounded-xl transition">
+        <div className="w-full flex justify-around items-center ">
+          <p className="font-semibold text-gray-800">120.00 LE</p>
+          <p className="border border-gray-600 rounded-2xl px-3 py-1 text-sm text-gray-600">
+            250g
+          </p>
+        </div>
+
+        <button className="w-full flex items-center justify-center gap-2 bg-button-accent hover:bg-button-accent-hover rounded-xl text-white px-4 py-2 transition">
           <ShoppingCart size={20} />
           Buy Now
         </button>
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { MoveRight } from "lucide-react";
 import Cards from "./_components/Cards";
 import HeroSection from "./_components/HeroSection";
 import Section from "./_components/Section";
@@ -16,7 +17,7 @@ export default function Page() {
       {/* Shop Section */}
       <Section>
         <h2 className={`${bodoniModa.className} font-bold text-4xl py-6`}>
-          Shop
+          Our Coffee Selection
         </h2>
 
         <p className="text-gray-600 text-center mb-12">
@@ -25,6 +26,11 @@ export default function Page() {
         </p>
 
         <Cards />
+
+        <button className="mt-12 flex items-center justify-between gap-4 px-12 py-3 rounded-3xl text-button-primary border-button-primary border-2 sm:text-lg font-medium hover:bg-coffee-200 transition-all duration-300 hover:scale-105">
+          View All Products
+          <MoveRight size={20} />
+        </button>
       </Section>
     </>
   );
